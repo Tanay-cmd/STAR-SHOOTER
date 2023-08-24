6ff5366098628786a53911295211c5776893c71c
Tanay-cmd <81357948+Tanay-cmd@users.noreply.github.com>
2023-08-25 00:25:13 +0530

    Adapted code for new graphics

--- laser.py
+++ laser.py
@@ -1,9 +1,10 @@
 import pygame
 
 class Laser(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, speed, constraints):
+    def __init__(self, pos_x, pos_y, speed, constraints, number):
         super().__init__() 
-        self.image = pygame.image.load('Graphics//Charge_1.png')
+        path = 'Graphics//Charge_' + str(number) + '.png'
+        self.image = pygame.image.load(path)
         self.image = pygame.transform.rotozoom(self.image, 90, 0.4)
         self.rect = self.image.get_rect(midbottom=(pos_x, pos_y))
         self.speed = speed
